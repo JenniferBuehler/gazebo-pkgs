b6859e947e642f2139292cbb2f1f0e30011fc6a0
Jennifer Buehler <jennifer.e.buehler@gmail.com>
2020-12-07 14:23:09 +0100

    Add compatibility for gazebo 11 (default in noetic)

--- gazebo_version_helpers/include/gazebo_version_helpers/GazeboVersionHelpers.h
+++ gazebo_version_helpers/include/gazebo_version_helpers/GazeboVersionHelpers.h
@@ -7,13 +7,22 @@ namespace gazebo
 {
 
 // typedefs
-#if GAZEBO_MAJOR_VERSION >= 8
+#if GAZEBO_MAJOR_VERSION >= 11
 namespace gz_math = ignition::math;
 typedef gz_math::Pose3d GzPose3;
 typedef gz_math::Vector3d GzVector3;
 typedef gz_math::Quaterniond GzQuaternion;
 typedef gz_math::Matrix4d GzMatrix4;
 typedef gz_math::Matrix3d GzMatrix3;
+typedef gz_math::AxisAlignedBox GzBox;
+#elif GAZEBO_MAJOR_VERSION >= 8
+namespace gz_math = ignition::math;
+typedef gz_math::Pose3d GzPose3;
+typedef gz_math::Vector3d GzVector3;
+typedef gz_math::Quaterniond GzQuaternion;
+typedef gz_math::Matrix4d GzMatrix4;
+typedef gz_math::Matrix3d GzMatrix3;
+typedef gz_math::Box GzBox;
 #else
 namespace gz_math = gazebo::math;
 typedef gz_math::Pose GzPose3;
@@ -21,7 +30,9 @@ typedef gz_math::Vector3 GzVector3;
 typedef gz_math::Quaternion GzQuaternion;
 typedef gz_math::Matrix4 GzMatrix4;
 typedef gz_math::Matrix3 GzMatrix3;
+typedef gz_math::Box GzBox;
 #endif
+
 
 // Helper functions
 // //////////////////////////
@@ -116,7 +127,7 @@ std::string GetName(const T& t)
 
 ///////////////////////////////////////////////////////////////////////////////
 template<typename T>
-gz_math::Box GetBoundingBox(const T &t)
+GzBox GetBoundingBox(const T &t)
 {
 #if GAZEBO_MAJOR_VERSION >= 8
     return t.BoundingBox();
@@ -126,7 +137,7 @@ gz_math::Box GetBoundingBox(const T &t)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-GzVector3 GetBoundingBoxDimensions(const gz_math::Box &box);
+GzVector3 GetBoundingBoxDimensions(const GzBox &box);
 
 ///////////////////////////////////////////////////////////////////////////////
 template<typename T>
